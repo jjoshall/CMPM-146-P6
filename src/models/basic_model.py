49f4eae435022498f69635641fe713f4e65f7cf4
Josh Hall <148017202+jjoshall@users.noreply.github.com>
2025-02-17 20:20:10 -0800

    initial network built with 97k parameters but it is only 6% accurate lol

--- src/models/basic_model.py
+++ src/models/basic_model.py
@@ -1,15 +1,44 @@
 from models.model import Model
 from tensorflow.keras import Sequential, layers
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.layers import Rescaling
 from tensorflow.keras.optimizers import RMSprop, Adam
 
 class BasicModel(Model):
     def _define_model(self, input_shape, categories_count):
         # Your code goes here
         # you have to initialize self.model to a keras model
-        pass
+        
+        self.model = Sequential([
+            Rescaling(1./255, input_shape=input_shape),
+
+            layers.MaxPooling2D(2),
+
+            # 1st Convolutional Layer
+            layers.Conv2D(16, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(2),
+
+            # 2nd Convolutional Layer
+            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(2),
+
+            # 3rd Convolutional Layer
+            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(2),
+
+            # Flatten the results to feed into a DNN
+            layers.Flatten(),
+
+            # Dense Layer
+            layers.Dense(32, activation='relu'),
+            layers.Dense(categories_count, activation='softmax')
+        ])
     
     def _compile_model(self):
         # Your code goes here
         # you have to compile the keras model, similar to the example in the writeup
-        pass
+        
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
